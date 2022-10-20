1d41f25c57a4890cd6e7f69d8c6a1c134583115e
Karen Edith Moreno Moreno <kmorenom@credicorpcapital.com>
2022-10-20 17:43:27 -0500

    CRUD creado con python, este permite crear, modificar, listar, buscar y eliminar clientes

---
+++ Ventas/main.py
@@ -0,0 +1,140 @@
+from dataclasses import field
+from http import client
+import sys
+
+from distutils.cmd import Command
+
+
+clients = [
+    {
+        'name': 'Karen Moreno',
+        'company': 'Google',
+        'email': 'karen@google.com',
+        'position': 'Sofware Engineer',
+    },
+    {
+        'name': 'Maria Lopez',
+        'company': 'Facebook',
+        'email': 'maria@facebook.com',
+        'position': 'Data Engineer',
+    },
+]
+
+
+def create_client(client):
+    global clients
+
+    if client not in clients:
+        clients.append(client) 
+    else:
+        print('Client already is in the client\'s list')
+
+
+def list_clients():
+    for idx, client in enumerate(clients):
+        print('{uid} | {name} | {company} | {email} | {position}'.format(
+           uid = idx,
+           name = client['name'],
+           company = client['company'],
+           email = client['email'],
+           position = client['position'] 
+        ))
+
+
+def update_client(client_name, updated_name):
+    global clients
+
+    if client_name in clients:
+        index = clients.index(client_name)
+        clients[index] = updated_name
+    else:
+        print('Client is not in clients list')
+
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients.remove(client_name)
+    else:
+        print('Client is not in client\'s list')
+
+
+def search_client(client_name):
+    for client in clients:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
+def _print_welcome():
+    print('WELCOME TO VENTAS')
+    print('*' * 50)
+    print('What would you like to do day?')
+    print('[C]reate client')    
+    print('[L]ist client')
+    print('[U]pdate client')
+    print('[D]elete client')
+    print('[S]earch client')
+
+def _get_client_field(field_name):
+    field = None
+
+    while not field:
+        field = input('What is the client {}?: '.format(field_name))
+    return field
+
+
+def _get_client_name():
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name?: ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+        if not client_name:
+            sys.exit() 
+
+    return client_name
+
+
+if __name__ == '__main__':
+    _print_welcome()
+
+    command = input()
+    command = command.upper()
+
+    if command == 'C':
+        client = {
+           'name' : _get_client_field('name'),
+           'company' : _get_client_field('company'),
+           'email': _get_client_field('email'),
+           'position': _get_client_field('position') 
+        } 
+        create_client(client)
+        list_clients()
+    elif command == 'L':
+        list_clients()
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name?: ')
+        update_client(client_name, updated_client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
+    else:
+        print('Invalid command')
